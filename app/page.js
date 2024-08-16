6fdc288e4eb63ec998c0da95b499ce5011b32d61
SOURADEEP HAZRA <isouradeephazra@gmail.com>
2024-08-16 10:50:14 +0530

    Update signup/login ui

--- app/page.js
+++ app/page.js
@@ -1,11 +1,28 @@
-'use client'
+"use client";
 import Link from "next/link";
 
 export default function Home() {
+  const user =
+    typeof window !== "undefined" && JSON.parse(localStorage.getItem("user"));
+
+  if (!user) {
+    return (
+      <div className=" flex flex-col">
+        <p>Homepage</p>
+        <p>If you are a new user please sign up at first</p>
+        <Link href={"/signup"} className=" underline">
+          <p>Sign Up</p>
+        </Link>
+        <Link href={"/login"} className=" underline">
+          <p>Login</p>
+        </Link>
+      </div>
+    );
+  }
   return (
     <div className=" flex flex-col">
       <p>Homepage</p>
-      <Link href={"/project"}  className=" underline">
+      <Link href={"/project"} className=" underline">
         <p>Go to Project page</p>
       </Link>
       <Link href={"/task"} className=" underline">
